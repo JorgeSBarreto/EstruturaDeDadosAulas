f0f7dcca162657ac30164ca5e045dc2ab6f203b6
Jorge Barreto <jsambarreto@gmail.com>
2022-03-30 22:16:33 -0300

    ListaSimples

---
+++ src/ListaSimples/ListaSimples.java
@@ -0,0 +1,46 @@
+package ListaSimples;
+
+public class ListaSimples {
+	
+	private Nodo primeiro;
+	
+	public Nodo getPrimeiro() {
+		return primeiro;
+	}
+
+	public void setPrimeiro(Nodo primeiro) {
+		this.primeiro = primeiro;
+	}
+	
+	
+	public void adicionarNoInicio(float valor) {
+		
+		Nodo novoNodo = new Nodo();
+		novoNodo.setDado(valor);
+		
+		
+		novoNodo.setProximo(this.getPrimeiro());
+		
+		this.setPrimeiro(novoNodo);
+	}
+	
+	public void adicionarNoFinal(float valor) {
+		Nodo novoNodo = new Nodo();
+		novoNodo.setDado(valor);
+		
+		if (this.getPrimeiro()==null) {
+			this.setPrimeiro(novoNodo);
+		} else {
+		
+			Nodo nodoFinal = this.getPrimeiro();
+		
+			while (nodoFinal.getProximo()!=null) {
+				nodoFinal = nodoFinal.getProximo();
+				}
+			
+			nodoFinal.setProximo(novoNodo);
+			}
+	}
+		
+
+}
---
+++ src/ListaSimples/Main.java
@@ -0,0 +1,31 @@
+package ListaSimples;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Nodo elemento1 = new Nodo();
+		
+		elemento1.setDado(24);
+		
+		Nodo elemento2 = new Nodo();
+		
+		elemento2.setDado(12);
+		
+		elemento1.setProximo(elemento2);
+		
+		ListaSimples lista = new ListaSimples();
+		
+		lista.setPrimeiro(elemento1);
+		
+		System.out.println(lista.getPrimeiro().getDado());
+		
+		lista.adicionarNoInicio(34);
+		
+		System.out.println(lista.getPrimeiro().getDado());
+		
+		lista.adicionarNoFinal(35);
+		
+	}
+
+}
